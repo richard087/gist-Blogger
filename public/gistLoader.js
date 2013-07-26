6f6ebd017ee8248e9f0b968fe02c4edcce8d276c
Richard <richard.c1976+github@gmail.com>
2013-07-26 12:11:41 +1000

    Changed to my github account.

--- public/gistLoader.js
+++ public/gistLoader.js
@@ -1,6 +1,6 @@
 (function() {
     
-    var gistBloggerPath = "https://raw.github.com/moski/gist-Blogger/master/public/gistBlogger.js";
+    var gistBloggerPath = "https://raw.github.com/richard087/gist-Blogger/master/public/gistBlogger.js";
 
     // Initialize the load the main script.
     try {
